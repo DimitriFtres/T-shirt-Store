fe59e976fceaa9c12a96af244b70287af607f201
DimitriFtres <dimitrifastres@gmail.com>
2021-03-14 18:31:07 +0100

    Creation private part

--- ConnexionAdmin.php
+++ ConnexionAdmin.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     include("Head.php");
 ?>
 <body class="h-100 d-flex flex-column">
@@ -6,12 +7,21 @@
         include("Header.php");
     ?>
     <div class="d-flex my-auto flex-column align-items-center justify-content-center">
-        <p class="h1 text-center">Connexion</p>
-        <div class="background-light d-flex flex-column align-items-center">
+        <p class="h1 text-center mb-3">Connexion</p>
+        <div class="bg-light d-flex flex-column align-items-center">
             <img src="#" alt="logo t-shirt">
-            <p class="h2">Admin</p>
+            <p class="h2 mt-3">Admin</p>
+            <?php
+                if(isset($_SESSION["id"])){
+                    header("Location: Administration/AdminAccueil.php");
+                }else {
+                    if(isset($_GET["error"])){
+                        echo "<p class=\"\">Vos identifiants sont incorrects</p>";
+                    }
+                }
+            ?>
             <div class="">
-                <form class="d-flex flex-column" action="" method="POST">
+                <form class="d-flex flex-column" action="TraitementConnexion.php" method="POST">
                     <div class="d-flex justify-content-between my-2">
                         <label class="pr-2" for="email">Email :</label>
                         <input type="email" id="email" name="email">
@@ -20,7 +30,7 @@
                         <label class="pr-2" for="mdp">Mot de passe :</label>
                         <input type="password" id="mdp" name="mdp">
                     </div>
-                    <input class="btn mb-2 border" type="submit" value="Connexion">
+                    <input class="btn mt-2 border" type="submit" value="Connexion">
                 </form>
             </div> 
         </div>
---
+++ TraitementConnexion.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    include("Connexion_bdd.php");
+    $_POST["email"];
+    $_POST["mdp"];
+    print_r($_POST);
+
+    /*connexion a la base de donnée
+    comparé les données entre elles
+    si les données sont les memes creer une session*/
+    if((isset($_POST["email"])) AND (isset($_POST["mdp"]))){
+            if($admin = $bdd->prepare('SELECT ID FROM administrateurs 
+                                           WHERE Email = ? AND Password = ?
+                                           ')){
+                $admin -> execute(array($_POST["email"], $_POST["mdp"]));
+                if($admin -> rowCount() == 1){
+                    $_SESSION["id"] = $admin->fetch();
+                    header("Location: Administration/AdminAccueil.php");
+                }else{
+                    header("Location: ConnexionAdmin.php?error");
+                    exit;
+                }
+            }          
+        }else{
+            echo "<p class=\"h1 text-center my-5\">Vous vous êtes perdu ? Retournez sur la page d'acceuil <a href=\"index.php\"><\a></p>";
+        }
+        
+?>
