391960e16307c82748b699a7199d9026ef22f202
DimitriFtres <dimitrifastres@gmail.com>
2021-04-07 21:07:24 +0200

    Update Livraison

--- AdminGestionCommande.php
+++ AdminGestionCommande.php
@@ -7,6 +7,10 @@
     include("AdminHeader.html");
 ?>
 <?php
+    if((!empty($_POST)) AND (!empty($_GET))){
+        $nouvelEtat = $bdd->prepare("UPDATE commandes SET Etat_Livraison = ? WHERE ID = ?");
+        $nouvelEtat->execute(array($_POST["livrer"], $_GET["com"]));
+    }
     $ancienID = '';
     $commande = $bdd->query("SELECT c.ID AS commandeID, c.ID_Utilisateur, c.Date_Livraison, c.Etat_Livraison, c.Date_commande, u.id, CONCAT(u.Nom, ' ', u.prenom) as utilisateurNom, t.Numero_de_reference, t.Nom, t.prix FROM commandes AS c 
                              JOIN utilisateurs AS u ON u.id = c.ID_Utilisateur
@@ -16,14 +20,22 @@
     while($c = $commande ->fetch()){
         if($ancienID !== $c["commandeID"]){
             echo "<div class=\"d-flex justify-content-around mb-0 background align-items-center\">";
-            echo "<div class=\"col\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["commandeID"]."</p></div>";
-            echo "<div class=\"col\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["Date_commande"]."</p></div>";
-            echo "<div class=\"col\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["utilisateurNom"]."</p></div>";
-            echo "<div class=\"col\">
-                    <form action=\"\" method=\"POST\" class=\"pl-2 py-1 mb-0 color-light d-flex align-items-center justify-content-around\">
-                        <label for=\"livrer\" class=\"my-0\">Livrée</label>
-                        <input type=\"checkbox\" name=\"livrer\" id=\"livrer\" value=\"livrer\">
-                        <input type=\"submit\" class=\"btn btn-light py-0\">
+            echo "<div class=\"col-1\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["commandeID"]."</p></div>";
+            echo "<div class=\"col-3\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["Date_commande"]."</p></div>";
+            echo "<div class=\"col-3\"><p class=\"pl-2 py-1 mb-0 color-light\">".$c["utilisateurNom"]."</p></div>";
+            echo "<div class=\"col-5\">
+                    <form action=\"?com=".$c["commandeID"]."\" method=\"POST\" class=\"pl-2 py-1 mb-0 color-light d-flex align-items-center justify-content-around\">
+                        <select name=\"livrer\" id=\"livrer\">";
+                        $etatCommande = $bdd->query("SELECT ID, Nom FROM etat_livraison");
+                        while($e = $etatCommande->fetch()){
+                            echo "<option value=\"".$e["ID"]."\"";
+                            if($c["Etat_Livraison"] === $e["ID"]){
+                                echo "selected";
+                            }
+                            echo ">".$e["Nom"]."</option>";
+                        }
+                        echo "</select>";
+                        echo "<input type=\"submit\" class=\"btn btn-light py-0\">
                     </form>
                 </div>";
             echo "</div>";
