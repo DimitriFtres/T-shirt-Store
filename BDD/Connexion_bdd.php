928f285b2193d48c972597fd97f786f4b3aa0c38
DimitriFtres <78168134+DimitriFtres@users.noreply.github.com>
2021-06-02 11:03:25 +0200

    Update Connexion_bdd.php
    J'ai fait une bêtise

--- BDD/Connexion_bdd.php
+++ BDD/Connexion_bdd.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "185.98.131.109";
-    $user = "fastr1522145";
-    $password = "pcs33ctjva";
-    $table_name = "fastr1522145";
+    $host = "";
+    $user = "";
+    $password = "";
+    $table_name = "";
 try {
     $bdd = new PDO("mysql:host=$host; dbname=$table_name", $user, $password);
 } 
